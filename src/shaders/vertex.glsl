b4bd4b6f0787415244ea37f9ce4bd326d52e9e80
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-10-31 23:16:30 -0400

    Copy shader from galproject

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,21 +1,19 @@
 #version 330
 
-const vec3 verts[9] = vec3[9](vec3(1.0, 0.0, 0.0),
-                              vec3(0.0, 1.0, 0.0),
-                              vec3(0.0, 0.0, 0.0),
-                              vec3(0.0, 1.0, 0.0),
-                              vec3(0.0, 0.0, 1.0),
-                              vec3(0.0, 0.0, 0.0),
-                              vec3(0.0, 0.0, 1.0),
-                              vec3(1.0, 0.0, 0.0),
-                              vec3(0.0, 0.0, 0.0));
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec3 color;
 
-const vec4 colors[3] = vec4[3](vec4(1.0, 0.0, 0.0, 1.0),
-                               vec4(0.0, 1.0, 0.0, 1.0),
-                               vec4(0.0, 0.0, 1.0, 1.0));
-out vec4 v_color;
-uniform mat4 u_mvp;
-void main() {
-  v_color = colors[gl_VertexID / 3];
-  gl_Position = u_mvp * vec4(verts[gl_VertexID], 1.0);
+out vec4 vertNorm;
+out vec4 vertNormWorld;
+out vec3 vertColor;
+
+uniform mat4 mvpMat;
+
+void main()
+{
+  gl_Position = mvpMat * vec4(position, 1.0);
+  vertNorm      = normalize(mvpMat * vec4(normal, 0.0));
+  vertNormWorld = normalize(vertNorm);
+  vertColor     = color;
 }
