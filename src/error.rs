871ff3dbc7f386558ba3ff06ba5fc3ba95b747d6
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-11-09 15:31:56 -0500

    Debug for error

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,13 @@
-#[derive(Debug)]
+use std::fmt::Debug;
+
 pub enum Error {
     GLError(String),
+}
+
+impl Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::GLError(message) => write!(f, "OpenGL Error: {}", message),
+        }
+    }
 }
