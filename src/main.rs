80522a45747e41106611d47b84a141e865d3af91
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-11-16 08:47:47 -0500

    Pull out mesh visualization into another function

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use mesh::PolyMesh;
 use scene::CameraMouseControl;
 use std::{path::PathBuf, time::Instant};
 use three_d::{
-    degrees, vec3, AmbientLight, Camera, ClearState, CpuMaterial, CpuMesh, Cull, DirectionalLight,
-    FrameOutput, Gm, Indices, InnerSpace, InstancedMesh, Instances, Mat4, Mesh, PhysicalMaterial,
-    Positions, Quat, Srgba, Window, WindowSettings,
+    degrees, vec3, AmbientLight, Camera, ClearState, Context, CpuMaterial, CpuMesh, Cull,
+    DirectionalLight, FrameOutput, Gm, Indices, InnerSpace, InstancedMesh, Instances, Mat4, Mesh,
+    PhysicalMaterial, Positions, Quat, Srgba, Window, WindowSettings,
 };
 
 fn bunny_mesh() -> PolyMesh {
@@ -24,45 +24,16 @@ fn bunny_mesh() -> PolyMesh {
     mesh
 }
 
-pub fn main() {
-    let window = Window::new(WindowSettings {
-        title: "Viewer".to_string(),
-        min_size: (512, 256),
-        ..Default::default()
-    })
-    .unwrap();
-    let context = window.gl();
-    let mesh = {
-        // let mut mesh = PolyMesh::icosahedron(1.0).expect("Cannoto create icosahedron");
-        // let mut mesh = bunny_mesh();
-        let mut mesh = PolyMesh::unit_box().expect("Cannot make a box");
-        let before = Instant::now();
-        mesh.subdivide_loop(1, false).expect("Cannot do subd");
-        let duration = Instant::now() - before;
-        println!("Subdivision took {}ms", duration.as_millis());
-        println!(
-            "This mesh has {} boundary vertices.",
-            mesh.vertices().filter(|v| v.is_boundary(&mesh)).count()
-        );
-        mesh.update_face_normals()
-            .expect("Cannot update face normals");
-        mesh.update_vertex_normals_fast()
-            .expect("Cannot update vertex normals");
-        mesh
-    };
-    let target = vec3(0.0, 1.0, 0.0);
-    let scene_radius: f32 = 6.0;
-    let mut camera = Camera::new_perspective(
-        window.viewport(),
-        target + scene_radius * vec3(0.6, 0.3, 1.0).normalize(),
-        target,
-        vec3(0.0, 1.0, 0.0),
-        degrees(45.0),
-        0.1,
-        1000.0,
-    );
-    let mut control =
-        CameraMouseControl::new(*camera.target(), 0.1 * scene_radius, 100.0 * scene_radius);
+fn visualize_mesh(
+    mesh: &PolyMesh,
+    context: &Context,
+) -> (
+    Gm<Mesh, PhysicalMaterial>,
+    Gm<InstancedMesh, PhysicalMaterial>,
+    Gm<InstancedMesh, PhysicalMaterial>,
+) {
+    const CYL_RADIUS: f32 = 0.001;
+    const SPH_RADIUS: f32 = 0.002;
     // Create a CPU-side mesh consisting of a single colored triangle
     let (model, etransforms, vtransforms) = {
         let points = mesh.points();
@@ -100,7 +71,7 @@ pub fn main() {
                         let length = ev.magnitude();
                         ev /= length;
                         let ev = vec3(ev.x, ev.y, ev.z);
-                        let start = points[h.tail(&mesh).index() as usize];
+                        let start = points[h.tail(mesh).index() as usize];
                         let start = vec3(start.x, start.y, start.z);
                         Mat4::from_translation(start)
                             * Into::<Mat4>::into(Quat::from_arc(vec3(1.0, 0., 0.0), ev, None))
@@ -129,19 +100,72 @@ pub fn main() {
     );
     wireframe_material.render_states.cull = Cull::Back;
     let mut sphere = CpuMesh::sphere(8);
-    sphere.transform(&Mat4::from_scale(0.001)).unwrap();
+    sphere.transform(&Mat4::from_scale(SPH_RADIUS)).unwrap();
     let vertices = Gm::new(
         InstancedMesh::new(&context, &vtransforms, &sphere),
         wireframe_material.clone(),
     );
     let mut cylinder = CpuMesh::cylinder(10);
     cylinder
-        .transform(&Mat4::from_nonuniform_scale(1.0, 0.0005, 0.0005))
+        .transform(&Mat4::from_nonuniform_scale(1.0, CYL_RADIUS, CYL_RADIUS))
         .unwrap();
     let edges = Gm::new(
         InstancedMesh::new(&context, &etransforms, &cylinder),
         wireframe_material,
     );
+    (model, vertices, edges)
+}
+
+pub fn main() {
+    let window = Window::new(WindowSettings {
+        title: "Viewer".to_string(),
+        min_size: (512, 256),
+        ..Default::default()
+    })
+    .unwrap();
+    let context = window.gl();
+    let mesh = {
+        let mut mesh = PolyMesh::icosahedron(1.0).expect("Cannoto create icosahedron");
+        // let mut mesh = bunny_mesh();
+        // let mut mesh = PolyMesh::unit_box().expect("Cannot make a box");
+        let before = Instant::now();
+        mesh.subdivide_loop(3, true).expect("Cannot do subd");
+        let duration = Instant::now() - before;
+        println!("Subdivision took {}ms", duration.as_millis());
+        println!(
+            "This mesh has {} boundary vertices.",
+            mesh.vertices().filter(|v| v.is_boundary(&mesh)).count()
+        );
+        mesh.update_face_normals()
+            .expect("Cannot update face normals");
+        mesh.update_vertex_normals_fast()
+            .expect("Cannot update vertex normals");
+        mesh.check_topology().expect("Topological errors found");
+        mesh
+    };
+    let refbox = {
+        let mut mesh = PolyMesh::unit_box().expect("Cannot make a box");
+        mesh.update_face_normals()
+            .expect("Cannot update face normals");
+        mesh.update_vertex_normals_fast()
+            .expect("Cannot update vertex normals");
+        mesh
+    };
+    let target = vec3(0.0, 1.0, 0.0);
+    let scene_radius: f32 = 6.0;
+    let mut camera = Camera::new_perspective(
+        window.viewport(),
+        target + scene_radius * vec3(0.6, 0.3, 1.0).normalize(),
+        target,
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        1000.0,
+    );
+    let mut control =
+        CameraMouseControl::new(*camera.target(), 0.1 * scene_radius, 100.0 * scene_radius);
+    let (model, vertices, edges) = visualize_mesh(&mesh, &context);
+    let (_, rvs, res) = visualize_mesh(&refbox, &context);
     let ambient = AmbientLight::new(&context, 0.7, Srgba::WHITE);
     let directional0 = DirectionalLight::new(&context, 2.0, Srgba::WHITE, &vec3(-1.0, -1.0, -1.0));
     let directional1 = DirectionalLight::new(&context, 2.0, Srgba::WHITE, &vec3(1.0, 1.0, 1.0));
@@ -157,6 +181,7 @@ pub fn main() {
                 .render(
                     &camera,
                     model.into_iter().chain(&vertices).chain(&edges),
+                    // vertices.into_iter().chain(&edges).chain(&rvs).chain(&res),
                     &[&ambient, &directional0, &directional1],
                 );
         }
