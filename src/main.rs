419e4a6e243f2cd8c736376e455bc3f490ca3a62
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-01-18 17:48:39 -0500

    Render Meshes With three_d (#4)
    * Try to use custom adaptor with alum
    
    * Implement more adaptors
    
    * Keep up with the new stuff
    
    * Move bunny to function
    
    * Keep up with the new stuff
    
    * Tests to make sure the adaptors are working correctly
    
    * Clean up macro
    
    * Update dep
    
    * Test with subdivision
    
    * Benchmark subdivision
    
    * Test the new handle api
    
    * Test with the new iterators
    
    * Test loop subd
    
    * Pull out mesh visualization into another function
    
    * Test the fixed subdivision
    
    * Visualize the sqrt3 subdiv in openmesh
    
    * Test the sqrt subdivision
    
    * Rough sketch to test the decimation code
    
    * Partially successful test of the decimation framework
    
    * First successful test
    
    * Remove debugging code
    
    * Manually scroll through
    
    * Render edge colors
    
    * Clean everything up and first successful test
    
    * Test probabilistic quadric
    
    * Test the new property stuff
    
    * Update to render the mesh
    
    * Delete old shaders

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,41 @@
+mod mesh;
 mod scene;
 
-use alum::{element::Handle, PolyMeshF32};
+use alum::{Handle, HasIterators, HasTopology};
+use mesh::PolyMesh;
 use scene::CameraMouseControl;
+use std::path::PathBuf;
 use three_d::{
-    degrees, vec3, AmbientLight, Camera, ClearState, CpuMaterial, CpuMesh, Cull, DirectionalLight,
-    FrameOutput, Gm, Indices, InnerSpace, InstancedMesh, Instances, Mat4, Mesh, PhysicalMaterial,
-    Positions, Quat, Srgba, Window, WindowSettings,
+    degrees, vec3, AmbientLight, Camera, ClearState, Context, CpuMaterial, CpuMesh, Cull,
+    DirectionalLight, FrameOutput, Gm, Indices, InnerSpace, InstancedMesh, Instances, Mat4, Mesh,
+    PhysicalMaterial, Positions, Quat, Srgba, Window, WindowSettings,
 };
 
-pub fn main() {
-    let window = Window::new(WindowSettings {
-        title: "Viewer".to_string(),
-        min_size: (512, 256),
-        ..Default::default()
-    })
-    .unwrap();
-    let context = window.gl();
-    let mesh = {
-        let mut mesh = PolyMeshF32::icosahedron(1.0).expect("Cannoto create icosahedron");
-        // let mut mesh =
-        //     PolyMeshF32::load_obj(&PathBuf::from("/home/rnjth94/dev/alum/assets/bunny.obj"))
-        //         .expect("Cannot load obj");
-        // {
-        //     let mut points = mesh.points();
-        //     let mut points = points.try_borrow_mut().expect("Cannot borrow points");
-        //     for p in points.iter_mut() {
-        //         *p = *p * 10.; // Scale the mesh.
-        //     }
-        // }
-        mesh.update_face_normals()
-            .expect("Cannot update face normals");
-        mesh.update_vertex_normals_fast()
-            .expect("Cannot update vertex normals");
-        mesh
-    };
-    let target = vec3(0.0, 1.0, 0.0);
-    let scene_radius: f32 = 6.0;
-    let mut camera = Camera::new_perspective(
-        window.viewport(),
-        target + scene_radius * vec3(0.6, 0.3, 1.0).normalize(),
-        target,
-        vec3(0.0, 1.0, 0.0),
-        degrees(45.0),
-        0.1,
-        1000.0,
-    );
-    let mut control =
-        CameraMouseControl::new(*camera.target(), 0.1 * scene_radius, 100.0 * scene_radius);
+fn bunny_mesh() -> PolyMesh {
+    let mesh = PolyMesh::load_obj(&PathBuf::from(
+        "/home/rnjth94/dev/alum/assets/bunny_quad.obj",
+    ))
+    .expect("Cannot load obj");
+    {
+        let mut points = mesh.points();
+        let mut points = points.try_borrow_mut().expect("Cannot borrow points");
+        for p in points.iter_mut() {
+            *p = *p * 10.; // Scale the mesh.
+        }
+    }
+    mesh
+}
+
+fn visualize_mesh(
+    mesh: &PolyMesh,
+    context: &Context,
+) -> (
+    Gm<Mesh, PhysicalMaterial>,
+    Gm<InstancedMesh, PhysicalMaterial>,
+    Gm<InstancedMesh, PhysicalMaterial>,
+) {
+    const CYL_RADIUS: f32 = 0.005;
+    const SPH_RADIUS: f32 = 0.01;
     // Create a CPU-side mesh consisting of a single colored triangle
     let (model, etransforms, vtransforms) = {
         let points = mesh.points();
@@ -79,12 +68,12 @@ pub fn main() {
                 transformations: mesh
                     .edges()
                     .map(|e| {
-                        let h = mesh.edge_halfedge(e, false);
+                        let h = e.halfedge(false);
                         let mut ev = mesh.calc_halfedge_vector(h, &points);
-                        let length = ev.length();
+                        let length = ev.magnitude();
                         ev /= length;
                         let ev = vec3(ev.x, ev.y, ev.z);
-                        let start = points[mesh.from_vertex(h).index() as usize];
+                        let start = points[h.tail(mesh)];
                         let start = vec3(start.x, start.y, start.z);
                         Mat4::from_translation(start)
                             * Into::<Mat4>::into(Quat::from_arc(vec3(1.0, 0., 0.0), ev, None))
@@ -113,19 +102,54 @@ pub fn main() {
     );
     wireframe_material.render_states.cull = Cull::Back;
     let mut sphere = CpuMesh::sphere(8);
-    sphere.transform(&Mat4::from_scale(0.005)).unwrap();
+    sphere.transform(&Mat4::from_scale(SPH_RADIUS)).unwrap();
     let vertices = Gm::new(
         InstancedMesh::new(&context, &vtransforms, &sphere),
         wireframe_material.clone(),
     );
     let mut cylinder = CpuMesh::cylinder(10);
     cylinder
-        .transform(&Mat4::from_nonuniform_scale(1.0, 0.002, 0.002))
+        .transform(&Mat4::from_nonuniform_scale(1.0, CYL_RADIUS, CYL_RADIUS))
         .unwrap();
     let edges = Gm::new(
         InstancedMesh::new(&context, &etransforms, &cylinder),
         wireframe_material,
     );
+    (model, vertices, edges)
+}
+
+pub fn main() {
+    let window = Window::new(WindowSettings {
+        title: "Viewer".to_string(),
+        min_size: (512, 256),
+        ..Default::default()
+    })
+    .unwrap();
+    let context = window.gl();
+    let mesh = {
+        let mut mesh = bunny_mesh();
+        mesh.update_face_normals()
+            .expect("Cannot update face normals");
+        mesh.update_vertex_normals_fast()
+            .expect("Cannot update vertex normals");
+        mesh.check_topology().expect("Topological errors found");
+        mesh
+    };
+    let target = vec3(0.0, 1.0, 0.0);
+    let scene_radius: f32 = 6.0;
+    let mut camera = Camera::new_perspective(
+        window.viewport(),
+        target + scene_radius * vec3(0.6, 0.3, 1.0).normalize(),
+        target,
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        1000.0,
+    );
+    let mut control =
+        CameraMouseControl::new(*camera.target(), 0.1 * scene_radius, 100.0 * scene_radius);
+    let (model, vertices, edges) = visualize_mesh(&mesh, &context);
+    // let (_, rvs, res) = visualize_mesh(&refbox, &context);
     let ambient = AmbientLight::new(&context, 0.7, Srgba::WHITE);
     let directional0 = DirectionalLight::new(&context, 2.0, Srgba::WHITE, &vec3(-1.0, -1.0, -1.0));
     let directional1 = DirectionalLight::new(&context, 2.0, Srgba::WHITE, &vec3(1.0, 1.0, 1.0));
---
+++ src/mesh.rs
@@ -0,0 +1,225 @@
+use alum::DotProductAdaptor;
+use alum::{
+    Adaptor, CrossProductAdaptor, FloatScalarAdaptor, PolyMeshT, VectorAngleAdaptor,
+    VectorLengthAdaptor, VectorNormalizeAdaptor,
+};
+use three_d::{InnerSpace, Vec3};
+
+pub struct MeshAdaptor {}
+
+impl Adaptor<3> for MeshAdaptor {
+    type Vector = Vec3;
+
+    type Scalar = f32;
+
+    fn vector(coords: [Self::Scalar; 3]) -> Self::Vector {
+        three_d::vec3(coords[0], coords[1], coords[2])
+    }
+
+    fn zero_vector() -> Self::Vector {
+        three_d::vec3(0.0, 0.0, 0.0)
+    }
+
+    fn vector_coord(v: &Self::Vector, i: usize) -> Self::Scalar {
+        v[i]
+    }
+}
+
+impl FloatScalarAdaptor<3> for MeshAdaptor {
+    fn scalarf32(val: f32) -> Self::Scalar {
+        val
+    }
+
+    fn scalarf64(val: f64) -> Self::Scalar {
+        val as f32
+    }
+}
+
+impl VectorNormalizeAdaptor<3> for MeshAdaptor {
+    fn normalized_vec(v: Self::Vector) -> Self::Vector {
+        v.normalize()
+    }
+}
+
+impl CrossProductAdaptor for MeshAdaptor {
+    fn cross_product(a: Self::Vector, b: Self::Vector) -> Self::Vector {
+        a.cross(b)
+    }
+}
+
+impl DotProductAdaptor<3> for MeshAdaptor {
+    fn dot_product(a: Self::Vector, b: Self::Vector) -> Self::Scalar {
+        a.dot(b)
+    }
+}
+
+impl VectorLengthAdaptor<3> for MeshAdaptor {
+    fn vector_length(v: Self::Vector) -> Self::Scalar {
+        v.magnitude()
+    }
+}
+
+impl VectorAngleAdaptor for MeshAdaptor {
+    fn vector_angle(a: Self::Vector, b: Self::Vector) -> Self::Scalar {
+        a.angle(b).0
+    }
+}
+
+pub type PolyMesh = PolyMeshT<3, MeshAdaptor>;
+
+#[cfg(test)]
+mod test {
+    use super::PolyMesh;
+    use alum::{HasIterators, HasTopology};
+
+    macro_rules! assert_float_eq {
+        ($a:expr, $b:expr, $eps:expr, $debug:expr) => {{
+            // Make variables to avoid evaluating experssions multiple times.
+            let a = $a;
+            let b = $b;
+            let eps = $eps;
+            let error = (a - b).abs();
+            if error > eps {
+                eprintln!("{:?}", $debug);
+            }
+            assert!(
+                error <= eps,
+                "Assertion failed: |({}) - ({})| = {:e} <= {:e}",
+                a,
+                b,
+                error,
+                eps
+            );
+        }};
+        ($a:expr, $b:expr, $eps:expr) => {
+            assert_float_eq!($a, $b, $eps, "")
+        };
+        ($type:ty, $a:expr, $b:expr) => {
+            assert_float_eq!($type, $a, $b, $type::EPSILON)
+        };
+    }
+
+    macro_rules! assert_f32_eq {
+        ($a:expr, $b:expr, $eps:expr, $debug:expr) => {
+            assert_float_eq!($a, $b, $eps, $debug)
+        };
+        ($a:expr, $b:expr, $eps:expr) => {
+            assert_float_eq!($a, $b, $eps)
+        };
+        ($a:expr, $b:expr) => {
+            assert_float_eq!($a, $b, f32::EPSILON)
+        };
+    }
+
+    #[test]
+    fn t_quad_box() {
+        let qbox = PolyMesh::quad_box(three_d::vec3(0., 0., 0.), three_d::vec3(1., 1., 1.))
+            .expect("Cannot create a quad box mesh");
+        assert_eq!(qbox.num_vertices(), 8);
+        assert_eq!(qbox.num_halfedges(), 24);
+        assert_eq!(qbox.num_edges(), 12);
+        assert_eq!(qbox.num_faces(), 6);
+        for v in qbox.vertices() {
+            assert_eq!(qbox.vf_ccw_iter(v).count(), 3);
+        }
+        assert_eq!(1., qbox.try_calc_volume().expect("Cannot compute volume"));
+        assert_eq!(6., qbox.try_calc_area().expect("Cannot compute area"));
+    }
+
+    #[test]
+    fn t_tetrahedron() {
+        let tet = PolyMesh::tetrahedron(1.0).expect("Cannot create a tetrahedron");
+        assert_eq!(4, tet.num_vertices());
+        assert_eq!(12, tet.num_halfedges());
+        assert_eq!(6, tet.num_edges());
+        assert_eq!(4, tet.num_faces());
+        assert_eq!(
+            8.0 / 3.0f32.sqrt(),
+            tet.try_calc_area().expect("Cannot compute area")
+        );
+        assert_f32_eq!(
+            8.0 / (9.0 * 3.0f32.sqrt()),
+            tet.try_calc_volume().expect("Cannot compute volume")
+        );
+    }
+
+    #[test]
+    fn t_hexahedron() {
+        let hex = PolyMesh::hexahedron(1.0).expect("Cannot create hexahedron");
+        assert_eq!(hex.num_vertices(), 8);
+        assert_eq!(hex.num_halfedges(), 24);
+        assert_eq!(hex.num_edges(), 12);
+        assert_eq!(hex.num_faces(), 6);
+        assert_f32_eq!(8.0, hex.try_calc_area().expect("Cannot compute area"), 1e-6);
+        assert_f32_eq!(
+            8.0 / (3.0 * 3.0f32.sqrt()),
+            hex.try_calc_volume().expect("Cannot compute volume")
+        );
+    }
+
+    #[test]
+    fn t_octahedron() {
+        let oct = PolyMesh::octahedron(1.0).expect("Cannot create octahedron");
+        assert_eq!(oct.num_vertices(), 6);
+        assert_eq!(oct.num_halfedges(), 24);
+        assert_eq!(oct.num_edges(), 12);
+        assert_eq!(oct.num_faces(), 8);
+        assert_eq!(
+            4.0 * 3.0f32.sqrt(),
+            oct.try_calc_area().expect("Cannot compute area")
+        );
+        assert_f32_eq!(
+            4.0 / 3.0,
+            oct.try_calc_volume().expect("Cannot compute volume")
+        );
+    }
+
+    #[test]
+    fn t_icosahedron() {
+        let ico = PolyMesh::icosahedron(1.0).expect("Cannot create icosahedron");
+        assert_eq!(12, ico.num_vertices());
+        assert_eq!(60, ico.num_halfedges());
+        assert_eq!(30, ico.num_edges());
+        assert_eq!(20, ico.num_faces());
+        assert_f32_eq!(
+            {
+                let phi = (1.0 + 5.0f32.sqrt()) / 2.0;
+                20.0 * 3.0f32.sqrt() / (phi * phi + 1.0)
+            },
+            ico.try_calc_area().expect("Cannot compute area"),
+            1e-6
+        );
+        assert_f32_eq!(
+            {
+                let phi = (1.0 + 5.0f32.sqrt()) / 2.0;
+                20.0 * phi * phi / (3.0 * (phi * phi + 1.0) * (phi * phi + 1.0).sqrt())
+            },
+            ico.try_calc_volume().expect("Cannot compute volume"),
+            1e-6
+        );
+    }
+
+    #[test]
+    fn t_dodecahedron() {
+        let dod = PolyMesh::dodecahedron(1.0).expect("Cannot create dodecahedron");
+        assert_eq!(20, dod.num_vertices());
+        assert_eq!(60, dod.num_halfedges());
+        assert_eq!(30, dod.num_edges());
+        assert_eq!(12, dod.num_faces());
+        assert_f32_eq!(
+            {
+                let phi = (1.0 + 5.0f32.sqrt()) / 2.0;
+                20.0f32 / (phi * (3.0f32 - phi).sqrt())
+            },
+            dod.try_calc_area().expect("Cannot compute area"),
+            1e-6
+        );
+        assert_f32_eq!(
+            {
+                let phi = (1.0 + 5.0f32.sqrt()) / 2.0;
+                40.0 / (3.0 * 3.0f32.sqrt() * (6.0 - 2.0 * phi))
+            },
+            dod.try_calc_volume().expect("Cannot compute volume")
+        );
+    }
+}
